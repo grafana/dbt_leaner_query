4eae6f09362ac76eaee592a6469cc4da7ebd914f
Bob Samuels <bob.samuels@grafana.com>
2023-07-13 17:03:28 -0500

    add qualified table name

--- models/reports/rpt_bigquery_table_usage_daily.sql
+++ models/reports/rpt_bigquery_table_usage_daily.sql
@@ -61,7 +61,8 @@ dim_job_table_view_references as(
 ),
 
 unique_tables as (
-    select distinct referenced_view_or_table as table_name
+    select 
+        distinct referenced_view_or_table as table_name
     from dim_job_table_view_references
 ),
 
@@ -79,10 +80,10 @@ aggregates as (
     select
         calendar.date_day as report_date, -- calendar
         unique_tables.table_name, -- dim_job_table
-        unique_tables.project_id,
-        unique_tables.dataset_id,
-        unique_tables.table_or_view_id,
-        concat(unique_tables.project_id, '.', unique_tables.dataset_id, '.', unique_tables.table_or_view_id) as qualified_table_name,
+        dim_job_table_view_references.project_id,
+        dim_job_table_view_references.dataset_id,
+        dim_job_table_view_references.table_or_view_id,
+        dim_job_table_view_references.qualified_table_name,
         dim_job_table_view_references.layer_used as layer, -- dim_job_table
         dim_user_agents.client_type, -- dim_user_agents
         coalesce(count(fct_executed_statements.job_key), 0) as total_queries_run,  -- cnt(job_key) from fct_executed_statements
