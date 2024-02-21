887cdee26ece50f9ac1c7b3e04ece0841234ecb1
Brendan Duncan <brendanduncan34@gmail.com>
2024-02-21 08:48:07 -0500

    Fixing dbt/jinja syntax issue.

--- models/marts/dim_error_messages.sql
+++ models/marts/dim_error_messages.sql
@@ -17,7 +17,7 @@ with data_access as (
         and date(event_timestamp) >= current_date - 3
     {% endif %}
     {% if target.name == var('leaner_query_dev_target_name') and var('leaner_query_enable_dev_limits') %}
-        and date(event_timestamp) >= current_date - var('leaner_query_dev_limit_days')
+        and date(event_timestamp) >= current_date - {{ var('leaner_query_dev_limit_days') }}
     {% endif %}
 
 ),
